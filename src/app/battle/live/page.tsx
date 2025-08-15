b3b5b0e605a91f4e9efa19f9091c6ed2d22e0127
The Outland Tavern <JBEvans2000@gmail.com>
2025-08-15 22:03:54 +0000

    It looks like it auto muted when the autoplay started. Can you fix that?

--- src/app/battle/live/page.tsx
+++ src/app/battle/live/page.tsx
@@ -83,7 +83,7 @@ const getYouTubeEmbedUrl = (url: string) => {
     if (ampersandPosition !== -1) {
         videoId = videoId.substring(0, ampersandPosition);
     }
-    return `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`;
+    return `https://www.youtube.com/embed/${videoId}?autoplay=1`;
 };
 
 
@@ -378,3 +378,4 @@ export default function LiveBattlePage() {
     
 
     
+
