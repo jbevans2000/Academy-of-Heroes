6b64ec1dd4c41693da9b86126348846b22305b2e
The Outland Tavern <JBEvans2000@gmail.com>
2025-08-16 18:23:59 +0000

    change the font of the text "Or Login If You Have An Account"  and "Are

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -47,7 +47,7 @@ export default function LoginPage() {
                 <span className="w-full border-t" />
               </div>
               <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-card px-2 text-muted-foreground">
+                <span className="bg-card px-2 text-muted-foreground font-headline">
                   Or Login If You Have An Account
                 </span>
               </div>
@@ -60,7 +60,7 @@ export default function LoginPage() {
                 <span className="w-full border-t" />
               </div>
               <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-card px-2 text-muted-foreground">
+                <span className="bg-card px-2 text-muted-foreground font-headline">
                   Are you a teacher?
                 </span>
               </div>
