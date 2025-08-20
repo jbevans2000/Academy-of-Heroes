3ce80b112a4a44a36750f416c087a4758d587562
The Outland Tavern <JBEvans2000@gmail.com>
2025-08-20 13:39:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/teacher/dashboard/dashboard.tsx
+++ src/app/teacher/dashboard/dashboard.tsx
@@ -41,6 +41,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
