9b950cb86bb8b540f15d4babca3ddaf7bfe747d1
Jason Evans <support@academy-heroes.com>
2025-08-22 22:15:45 +0000

    Thats not what it is. None of the changes you just made had anything to

--- src/app/teacher/dashboard/dashboard.tsx
+++ src/app/teacher/dashboard/dashboard.tsx
@@ -41,6 +41,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -759,3 +760,5 @@ export default function Dashboard() {
     </div>
   );
 }
+
+    
