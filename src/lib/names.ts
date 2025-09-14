e866c8839de239a63858f7619fec27f97b915ce6
The Outland Tavern <JBEvans2000@gmail.com>
2025-09-14 02:07:37 +0000

    Here are the male names for the library:  Aerion Alderon Alistair Amroth

--- src/lib/names.ts
+++ src/lib/names.ts
@@ -1,18 +1,28 @@
 
 export const maleNames = [
-    'Aldric', 'Gideon', 'Kael', 'Ronan', 'Theron', 'Bram', 'Fendrel', 'Corbin', 
-    'Orin', 'Lysander', 'Tarek', 'Garrick', 'Zephyr', 'Roric', 'Finnian', 
-    'Peregrine', 'Darian', 'Valerius', 'Caspian', 'Evander', 'Leoric', 'Torvin', 
-    'Silas', 'Joric', 'Hadrian', 'Dax', 'Kaelen', 'Zane', 'Rylan', 'Eamon', 
-    'Gareth', 'Lucian', 'Brand', 'Stellan', 'Roric', 'Alaric', 'Gawain', 
-    'Perrin', 'Torin', 'Niall', 'Kieran', 'Declan', 'Ronan', 'Cullen', 
-    'Ferris', 'Quinlan', 'Soren', 'Tavish', 'Riordan', 'Malachi', 'Brennus', 
-    'Cedric', 'Donnel', 'Ewan', 'Fergus', 'Galen', 'Hamish', 'Ivor', 'Jarl', 
-    'Lachlan', 'Magnus', 'Oisin', 'Padraig', 'Rory', 'Struan', 'Turlough', 
-    'Ulf', 'Valdis', 'Wulfric', 'Yorick', 'Zarek', 'Arion', 'Bastian', 
-    'Cassius', 'Dorian', 'Elian', 'Fabian', 'Griffin', 'Hector', 'Icarus', 
-    'Jasper', 'Kai', 'Lazarus', 'Marius', 'Nikolai', 'Orion', 'Phineas', 
-    'Remus', 'Sebastian', 'Titus', 'Uriah', 'Victor', 'Xerxes', 'Yancy', 'Zion'
+    'Aerion', 'Alderon', 'Alistair', 'Amroth', 'Aranel', 'Arawin', 'Arion', 
+    'Arlen', 'Arminas', 'Arod', 'Artanis', 'Asriel', 'Baelorn', 'Baldor', 
+    'Barion', 'Beleg', 'Beorn', 'Beric', 'Beren', 'Brimir', 'Caelan', 
+    'Cairn', 'Calian', 'Celeborn', 'Celerin', 'Ciaran', 'Cirdan', 'Colborn', 
+    'Daeron', 'Dairon', 'Dalion', 'Daran', 'Darien', 'Darian', 'Davion', 'Deor', 
+    'Dior', 'Diren', 'Eamon', 'Eardan', 'Elanor', 'Eldrin', 'Elric', 'Elrond', 
+    'Elowen', 'Elyn', 'Emeric', 'Eomer', 'Erion', 'Eriol', 'Erland', 'Errol', 
+    'Faelar', 'Faelan', 'Faran', 'Faramir', 'Fearen', 'Finnian', 'Finnin', 
+    'Finwë', 'Forion', 'Galdor', 'Gareth', 'Garion', 'Gildor', 'Gilion', 
+    'Gimli', 'Glorfindel', 'Gorwen', 'Grael', 'Gretel', 'Hadrian', 'Haldir', 
+    'Haldor', 'Hamilcar', 'Harion', 'Havelock', 'Helion', 'Helya', 'Heron', 
+    'Hildor', 'Hilor', 'Hoel', 'Iarwain', 'Idril', 'Ilian', 'Ilyan', 'Imrahil', 
+    'Inglor', 'Iorwerth', 'Isildor', 'Ithildin', 'Jaelen', 'Jaron', 'Jethro', 
+    'Kairn', 'Kalon', 'Kieran', 'Laelon', 'Laelth', 'Landon', 'Larion', 
+    'Laurion', 'Lian', 'Lindir', 'Lindon', 'Lirael', 'Lorien', 'Lucian', 
+    'Lysander', 'Maedhros', 'Maglor', 'Malador', 'Malek', 'Mairon', 'Manwë', 
+    'Maron', 'Meldor', 'Meriadoc', 'Milan', 'Mirkwood', 'Morwen', 'Naerion', 
+    'Niel', 'Nimrodel', 'Ohtar', 'Olorin', 'Orion', 'Oron', 'Osric', 'Peregrin', 
+    'Rannon', 'Rhys', 'Riel', 'Rion', 'Roeland', 'Rogan', 'Roderick', 'Saelon', 
+    'Salador', 'Samwise', 'Silas', 'Sorin', 'Taran', 'Telcontar', 'Thranduil', 
+    'Thurin', 'Tiernan', 'Tilion', 'Torvin', 'Turgon', 'Urial', 'Valandil', 
+    'Valen', 'Varian', 'Vartan', 'Vigo', 'Vingilot', 'Voronwe', 'Wain', 
+    'Wilder', 'Will', 'Yaron', 'Ywain', 'Zorian'
 ];
 
 export const femaleNames = [
@@ -23,8 +33,7 @@ export const femaleNames = [
     'Elara', 'Elenia', 'Liora', 'Amaryllis', 'Rosalind', 'Luthien', 'Eluned', 
     'Isadora', 'Alanis', 'Laurinda', 'Seraphina', 'Anya', 'Faelarion', 'Nimue', 
     'Lyra', 'Elisif', 'Taelynn', 'Lian', 'Arianwen', 'Avelina', 'Lyrie', 'Rhianna', 
-    'Caera', 'Lirina', 'Isolde', 'Elisabet', 'Aline', 'Silverlyn', 'Faelara', 
-    'Aurelia', 'Talia', 'Laelith', 'Aerin', 'Brighid', 'Lianne', 'Annalise', 
-    'Laural', 'Silverleaf', 'Liraleen', 'Avelynn', 'Caelinn', 'Taelyr', 'Arianrhod', 
-    'Rhiann'
+    'Caera', 'Lirina', 'Isolde', 'Elisabet', 'Aline', 'Silverlyn', 'Aurelia', 'Talia', 
+    'Laelith', 'Aerin', 'Brighid', 'Lianne', 'Annalise', 'Laural', 'Silverleaf', 
+    'Liraleen', 'Avelynn', 'Caelinn', 'Taelyr', 'Arianrhod', 'Rhiann'
 ];
