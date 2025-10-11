4612ad19297bde7754946bf73e09cca83fd1a749
The Outland Tavern <JBEvans2000@gmail.com>
2025-10-11 14:05:43 +0000

    Silvaria 4: (Bottom-Left) Chromosome #4
    Wing Style
    Number of Toes

--- src/app/teacher/tools/genetics-lab/page.tsx
+++ src/app/teacher/tools/genetics-lab/page.tsx
@@ -306,7 +306,7 @@ function GeneticsLabContent() {
                 `Chromosome #1\n\nEye Color - ee\nSpikes - ss\nHorns - Hh`,
                 `Chromosome #2\n\nTail Length\nArmored Belly\nClawed Wings`,
                 `Chromosome #3\n\nBody Color\nFire Breathing\nNeck Length`,
-                '',
+                `Chromosome #4  \n\nWing Style\nNumber of Toes`,
                 '',
                 ''
             ];
